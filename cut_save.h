9fea28310990e9186833b61aa5b3f63ddbfb1f9e
jisu <121723740+usseez@users.noreply.github.com>
2025-12-29 10:37:05 +0900

    Add files via upload

---
+++ cut_save.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef FUNTION_H
+#define FUNCTION_H
+
+#define MAX_VIDEO_NUM 10
+#define MAX_LENGTH 256
+
+#ifdef __cplusplus
+        extern"C" {
+#endif
+
+//     extern void
+
+#ifdef __cplusplus
+
+        } /*end of the 'externC' block*/
+#endif
+
+
+
+#endif
